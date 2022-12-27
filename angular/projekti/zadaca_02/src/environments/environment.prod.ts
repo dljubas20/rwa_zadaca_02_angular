25ebdc920d72c38a0cc2a6c0df3a6e1835de53f5
Dario Ljubas <darioljubas33@gmail.com>
2022-12-27 23:00:38 +0100

    Dodane datoteke environment, dodan folder slike u assets

---
+++ angular/projekti/zadaca_02/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    restServis: "http://spider.foi.hr:12220/api/",
+    posteriPutanja: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/"
+}
---
+++ angular/projekti/zadaca_02/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    restServis: "http://spider.foi.hr:12220/api/",
+    posteriPutanja: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/"
+}
