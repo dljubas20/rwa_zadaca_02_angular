8fe27a7eb0d1d2c2fcdb916194a0eedfdf6ec22c
Dario Ljubas <darioljubas33@gmail.com>
2022-12-28 16:02:51 +0100

    Dodani restPort i appPort u environment datoteke

--- angular/projekti/zadaca_02/src/environments/environment.prod.ts
+++ angular/projekti/zadaca_02/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    restServis: "http://spider.foi.hr:12220/api/",
+    restPort: "12259",
+    appPort: "12112",
     posteriPutanja: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/"
 }
