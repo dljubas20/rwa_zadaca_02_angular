9fc214c6d9e901c8f87543eb2edf930bb74c554c
Dario Ljubas <darioljubas33@gmail.com>
2022-12-28 15:26:38 +0100

    Generirane ostale komponente koje će predstavljati stranice

---
+++ angular/projekti/zadaca_02/src/app/filmovi-pregled/filmovi-pregled.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filmovi-pregled',
+  templateUrl: './filmovi-pregled.component.html',
+  styleUrls: ['./filmovi-pregled.component.scss']
+})
+export class FilmoviPregledComponent {
+
+}
---
+++ angular/projekti/zadaca_02/src/app/pretrazivanje-filmova/pretrazivanje-filmova.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pretrazivanje-filmova',
+  templateUrl: './pretrazivanje-filmova.component.html',
+  styleUrls: ['./pretrazivanje-filmova.component.scss']
+})
+export class PretrazivanjeFilmovaComponent {
+
+}
