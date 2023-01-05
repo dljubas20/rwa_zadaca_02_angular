358b70ed2201e42111e6a88d4a64b48e991a5958
Dario Ljubas <darioljubas33@gmail.com>
2023-01-05 01:30:46 +0100

    Stvoren novi dijalog za ažuriranje žanra

---
+++ angular/projekti/zadaca_02/src/app/zanrovi/zanrovi-azuriraj-dijalog/zanrovi-azuriraj-dijalog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zanrovi-azuriraj-dijalog',
+  templateUrl: './zanrovi-azuriraj-dijalog.component.html',
+  styleUrls: ['./zanrovi-azuriraj-dijalog.component.scss']
+})
+export class ZanroviAzurirajDijalogComponent {
+
+}
