a8fddf80c5a8cf60f0a5672efc28ff222dc9e39f
Dario Ljubas <darioljubas33@gmail.com>
2023-01-10 22:55:25 +0100

    Dovršena dokumentacija, maknute viška datoteke, maknut TMDB API ključ

--- angular/projekti/zadaca_02/src/app/komponente/dokumentacija/dokumentacija.component.ts
+++ angular/projekti/zadaca_02/src/app/komponente/dokumentacija/dokumentacija.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'projekti/zadaca_02/src/environments/environment';
 
 @Component({
   selector: 'app-dokumentacija',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./dokumentacija.component.scss']
 })
 export class DokumentacijaComponent {
-
+  appPort = environment.appPort;
+  restPort = environment.restPort;
 }
